62d869f921a2ec6a1ee5508555a2c6d6d2a88cea
William Chong <me@williamchong.cloud>
2021-06-26 04:33:34 +0800

    ✨ Add keplr and transaction patch

--- store/keplr.js
+++ store/keplr.js
@@ -55,7 +55,7 @@ export const actions = {
           stakeCurrency: lunieCoinToKeplrCoin(network.stakingDenom),
           // (Optional) If you have a wallet webpage used to stake the coin then provide the url to the website in `walletUrlForStaking`.
           // The 'stake' button in Keplr extension will link to the webpage.
-          // walletUrlForStaking: "",
+          walletUrlForStaking: network.stakingWalletURL,
           // The BIP44 path.
           bip44: {
             // You can only set the coin type of BIP44.
@@ -140,6 +140,6 @@ function lunieCoinToKeplrCoin(denom) {
       .split('.')[1].length,
     // (Optional) Keplr can show the fiat value of the coin if a coingecko id is provided.
     // You can get id from https://api.coingecko.com/api/v3/coins/list if it is listed.
-    // coinGeckoId: ""
+    coinGeckoId: coinLookup.coinGeckoId,
   }
 }
