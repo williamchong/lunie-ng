e7722ccb7a5771900acbe3c62178204260846b64
William Chong <me@williamchong.cloud>
2021-07-28 21:34:33 +0800

    🔧 Add LikeCoin public test settings

--- network.js
+++ network.js
@@ -1,21 +1,20 @@
 export default {
-  id: 'likecoin-chain-sheungwan',
-  name: 'LikeCoin chain',
+  id: 'likecoin-public-testnet-3', // DEPRECATE, only used for Lunie extension, NOT CHAIN ID
+  name: 'LikeCoin public test chain',
   description:
     'LikeCoin is a decentralized publishing infrastructure. It provides a comprehensive metadata framework to facilitate content registration, licensing, and monetization for all media types.',
   logo: `logo.svg`,
-  website: 'https://like.co',
+  website: 'https://rinkeby.like.co',
   siteURL: 'https://stake.like.co',
-  apiURL: 'https://mainnet-node.like.co', // use `npx lcp --proxyUrl http://34.123.30.100:1317`
-  rpcURL: 'ws://mainnet-node.like.co:26657',
-  stakingWalletURL: 'https://stake.like.co',
+  apiURL: 'https://likecoin-public-testnet-lcd.nnkken.dev/', // use `npx lcp --proxyUrl http://34.123.30.100:1317`
+  rpcURL: 'https://likecoin-public-testnet-lcd.nnkken.dev/',
   authcoreURL: 'https://authcore.like.co',
-  minBlockHeight: 1,
-  stakingDenom: 'LIKE',
+  stakingWalletURL: 'https://stake.like.co',
+  stakingDenom: 'EKIL',
   coinLookup: [
     {
-      viewDenom: 'LIKE',
-      chainDenom: 'nanolike',
+      viewDenom: 'EKIL',
+      chainDenom: 'nanoekil',
       chainToViewConversionFactor: '0.000000001',
       icon: `currencies/like.png`,
       coinGeckoId: 'likecoin',
@@ -31,7 +30,7 @@ export default {
       gasEstimate: 350000,
       feeOptions: [
         {
-          denom: 'LIKE',
+          denom: 'EKIL',
           amount: 0.035,
         },
       ],
