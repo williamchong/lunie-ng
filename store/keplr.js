73d75a8558a0c662045b36aa4bc987bbc66f0cfe
William Chong <me@williamchong.cloud>
2021-10-16 01:25:27 +0800

    🔧 Add ibc flag in keplr config

--- store/keplr.js
+++ store/keplr.js
@@ -43,35 +43,15 @@ export const actions = {
         // If the user rejects it or the suggested chain information doesn't include the required fields, it will throw an error.
         // If the same chain id is already registered, it will resolve and not require the user interactions.
         await window.keplr.experimentalSuggestChain({
-          // Chain-id of the Cosmos SDK chain.
           chainId: block.chainId,
-          // The name of the chain to be displayed to the user.
           chainName: network.name,
-          // RPC endpoint of the chain.
           rpc: network.rpcURL,
-          // REST endpoint of the chain.
           rest: network.apiURL,
-          // Staking coin information
           stakeCurrency: lunieCoinToKeplrCoin(network.stakingDenom),
-          // (Optional) If you have a wallet webpage used to stake the coin then provide the url to the website in `walletUrlForStaking`.
-          // The 'stake' button in Keplr extension will link to the webpage.
           walletUrlForStaking: network.stakingWalletURL,
-          // The BIP44 path.
           bip44: {
-            // You can only set the coin type of BIP44.
-            // 'Purpose' is fixed to 44.
             coinType: 118,
           },
-          // Bech32 configuration to show the address to user.
-          // This field is the interface of
-          // {
-          //   bech32PrefixAccAddr: string;
-          //   bech32PrefixAccPub: string;
-          //   bech32PrefixValAddr: string;
-          //   bech32PrefixValPub: string;
-          //   bech32PrefixConsAddr: string;
-          //   bech32PrefixConsPub: string;
-          // }
           bech32Config: {
             bech32PrefixAccAddr: network.addressPrefix,
             bech32PrefixAccPub: network.addressPrefix + 'pub',
@@ -80,29 +60,19 @@ export const actions = {
             bech32PrefixConsAddr: network.addressPrefix + 'valcons',
             bech32PrefixConsPub: network.addressPrefix + 'valconspub',
           },
-          // List of all coin/tokens used in this chain.
           currencies: network.coinLookup.map(({ viewDenom }) =>
             lunieCoinToKeplrCoin(viewDenom)
           ),
-          // List of coin/tokens used as a fee token in this chain.
           feeCurrencies: network.coinLookup.map(({ viewDenom }) =>
             lunieCoinToKeplrCoin(viewDenom)
           ),
-          // (Optional) The number of the coin type.
-          // This field is only used to fetch the address from ENS.
-          // Ideally, it is recommended to be the same with BIP44 path's coin type.
-          // However, some early chains may choose to use the Cosmos Hub BIP44 path of '118'.
-          // So, this is separated to support such chains.
           coinType: 118,
-          // (Optional) This is used to set the fee of the transaction.
-          // If this field is not provided, Keplr extension will set the default gas price as (low: 0.01, average: 0.025, high: 0.04).
-          // Currently, Keplr doesn't support dynamic calculation of the gas prices based on on-chain data.
-          // Make sure that the gas prices are higher than the minimum gas prices accepted by chain validators and RPC/REST endpoint.
-          // gasPriceStep: {
-          //     low: 0.01,
-          //     average: 0.025,
-          //     high: 0.04
-          // }
+          gasPriceStep: {
+            low: 0.01,
+            average: 10,
+            high: 1000,
+          },
+          features: ['stargate', 'ibc-transfer'],
         })
         await window.keplr.enable(block.chainId)
 
